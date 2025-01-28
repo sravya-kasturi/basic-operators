b6f1d804738c918d5332c65b9bb97e0b328d55f7
KASTURI SRAVYA <kasturisravya15@gmail.com>
2025-01-28 16:06:11 +0530

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,32 @@
+public class Operator {
+    public static void main(String[] args){
+               
+                int num1 = 10;
+                int num2 = 3;
+        
+                System.out.println("Arithmetic Operators:");
+                System.out.println("num1 + num2 = " + (num1 + num2));
+                System.out.println("num1 - num2 = " + (num1 - num2));
+                System.out.println("num1 * num2 = " + (num1 * num2));
+                System.out.println("num1 / num2 = " + (num1 / num2));
+                System.out.println("num1 % num2 = " + (num1 % num2));
+        
+                // Relational Operators
+                System.out.println("\nRelational Operators:");
+                System.out.println("num1 == num2 = " + (num1 == num2));
+                System.out.println("num1 != num2 = " + (num1 != num2));
+                System.out.println("num1 > num2 = " + (num1 > num2));
+                System.out.println("num1 < num2 = " + (num1 < num2));
+                System.out.println("num1 >= num2 = " + (num1 >= num2));
+                System.out.println("num1 <= num2 = " + (num1 <= num2));
+        
+                // Logical Operator
+                boolean bool1 = true;
+                boolean bool2 = false;
+        
+                System.out.println("\nLogical Operators:");
+                System.out.println("bool1 && bool2 = " + (bool1 && bool2));
+                System.out.println("bool1 || bool2 = " + (bool1 || bool2));
+                System.out.println("!bool1 = " + (!bool1));
+        }
+     } 
